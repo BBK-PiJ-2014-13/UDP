cd393c523b889104ea87cadbcb0a32f621f9ea28
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-20 18:59:24 +0000

    Added hostName variable

--- src/core/Client.java
+++ src/core/Client.java
@@ -1,11 +1,22 @@
 package core;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
 	public static void main(String[] args) {
+		String hostName = "host";
 		int portNumber = 4444;
 		
-		Socket socket = new Socket(hostName, portNumber);
+		try {
+			Socket socket = new Socket(hostName, portNumber);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
