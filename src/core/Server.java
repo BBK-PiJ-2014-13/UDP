f9953b56434e485f35fe838aa17b746e0aa9d126
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-20 16:58:23 +0000

    Added serverSocket variable

--- src/core/Server.java
+++ src/core/Server.java
@@ -1,8 +1,18 @@
 package core;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
 	public static void main(String[] args) {
 		int portNumber = 4444;
 		boolean listening = true;
+		
+		try {
+			ServerSocket serverSocket = new ServerSocket(portNumber);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
