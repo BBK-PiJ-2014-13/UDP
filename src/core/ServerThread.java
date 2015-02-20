2f1ddda1467de13f8de6099b0251a870da12e088
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-20 17:19:29 +0000

    Added run() method

--- src/core/ServerThread.java
+++ src/core/ServerThread.java
@@ -4,10 +4,14 @@ import java.net.Socket;
 
 public class ServerThread extends Thread {
 	private Socket socket = null;
-	
+
 	public ServerThread(Socket socket) {
 		super("ServerThread");
 		this.socket = socket;
+	}
+
+	public void run() {
+
 	}
 
 }
