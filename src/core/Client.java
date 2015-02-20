55a863080f2500142c6db1d6882f6f2381e0c14c
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-20 19:20:51 +0000

    Added comments

--- src/core/Client.java
+++ src/core/Client.java
@@ -13,6 +13,8 @@ public class Client {
 			Socket socket = new Socket(hostName, portNumber);
 			// TODO Ask for Unique ID
 			// TODO Ask if its first to connect
+			// TODO Open UDP connection to server
+				// Send or receive audio
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
