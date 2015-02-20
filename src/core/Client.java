610c917d08724bef31bdb0450fd5e4813b65b218
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-20 17:55:41 +0000

    Added Socket variable

--- src/core/Client.java
+++ src/core/Client.java
@@ -1,7 +1,11 @@
 package core;
 
+import java.net.Socket;
+
 public class Client {
 	public static void main(String[] args) {
 		int portNumber = 4444;
+		
+		Socket socket = new Socket(hostName, portNumber);
 	}
 }
