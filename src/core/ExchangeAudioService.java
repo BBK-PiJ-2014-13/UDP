7dbccad34f1531ea3c36faec27b57703aef2014b
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-21 16:42:56 +0000

    Added method exchangeAudio()

--- src/core/ExchangeAudioService.java
+++ src/core/ExchangeAudioService.java
@@ -1,7 +1,10 @@
 package core;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface ExchangeAudioService extends Remote {
+	
+	public void exchangeAudio() throws RemoteException;
 
 }
