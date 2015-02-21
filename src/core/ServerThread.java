d676eea9a2900511a413931f2007f2b0e1ac93fc
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-21 17:46:01 +0000

    Added readers and datastream to run() method

--- src/core/ServerThread.java
+++ src/core/ServerThread.java
@@ -1,10 +1,14 @@
 package core;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ServerThread extends Thread {
 	private Socket socket = null;
+	String clientSentence = null;
 
 	public ServerThread(Socket socket) {
 		super("ServerThread");
@@ -13,6 +17,16 @@ public class ServerThread extends Thread {
 
 	public void run() {
 		// TODO Send unique ID
+		try {
+			BufferedReader inFromClient = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(
+					socket.getOutputStream());
+			clientSentence = inFromClient.readLine();
+			System.out.println("Received: " + clientSentence);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 		// TODO Tell client if its sender or receiver
 		// TODO Listen to UDP connection
 		// TODO Tell client to connect over UDP
