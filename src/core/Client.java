d6afaee817cf9dbb87eb9df6a89c3b255507511e
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-21 17:00:38 +0000

    Added method ServerLink()

--- src/core/Client.java
+++ src/core/Client.java
@@ -1,5 +1,19 @@
 package core;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Client {
+	public void ServerLink() {
+		try {
+			Registry reg = LocateRegistry.getRegistry("localhost", 1099);
+			ExchangeAudioService exchangeAudio = (ExchangeAudioService) reg.lookup("audioExchange");
+			exchangeAudio.exchangeAudio();
+		} catch (RemoteException | NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
