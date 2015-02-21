0c1b8656e8b27cf16b2e4bb3b81bd33b4a6976e2
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-21 18:48:13 +0000

    Added tracking of ID

--- src/core/Server.java
+++ src/core/Server.java
@@ -7,11 +7,13 @@ public class Server {
 	public static void main(String[] args) {
 		int portNumber = 4444;
 		boolean listening = true;
+		int nextID = 0;
 		
 		try {
 			ServerSocket serverSocket = new ServerSocket(portNumber);
 			while (listening) {
-				new ServerThread(serverSocket.accept()).start();
+				new ServerThread(serverSocket.accept(), nextID).start();
+				nextID++;
 			}
 		} catch (IOException e) {
 			System.err.println("Could not listen on port " + portNumber);
