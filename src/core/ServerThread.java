ba4b1880322a9f14f4721b54b41243fd7d36acb1
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-22 17:45:46 +0000

    Added audio file

--- src/core/ServerThread.java
+++ src/core/ServerThread.java
@@ -39,33 +39,30 @@ public class ServerThread extends Thread {
 			if (inFromClient.readLine() == "firstToConnectRequest") {
 				outToClient.writeBytes(Boolean.toString(isFirstToConnect));
 			}
-			
+
 			// TODO Listen to UDP connection
 			DatagramSocket serverSocket = new DatagramSocket(4444);
 			byte[] receiveData = new byte[1024];
-            byte[] sendData = new byte[1024];
-            while(true) {
-            	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            	serverSocket.receive(receivePacket);
-            	String sentence = new String( receivePacket.getData());
-                System.out.println("RECEIVED: " + sentence);
-                InetAddress IPAddress = receivePacket.getAddress();
-                int port = receivePacket.getPort();
-                String capitalizedSentence = sentence.toUpperCase();
-                sendData = capitalizedSentence.getBytes();
-                DatagramPacket sendPacket =
-                new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                serverSocket.send(sendPacket);
-            }
+			byte[] sendData = new byte[1024];
+			while (true) {
+				DatagramPacket receivePacket = new DatagramPacket(receiveData,
+						receiveData.length);
+				serverSocket.receive(receivePacket);
+				String sentence = new String(receivePacket.getData());
+				System.out.println("RECEIVED: " + sentence);
+				InetAddress IPAddress = receivePacket.getAddress();
+				int port = receivePacket.getPort();
+				String capitalizedSentence = sentence.toUpperCase();
+				sendData = capitalizedSentence.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData,
+						sendData.length, IPAddress, port);
+				serverSocket.send(sendPacket);
+			}
 
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 
-		
-		
-		// TODO Tell client to connect over UDP
-		// TODO Relay Audio data
 		try {
 			socket.close();
 		} catch (IOException e) {
