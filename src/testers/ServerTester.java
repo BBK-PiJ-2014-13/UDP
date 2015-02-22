413e77e3e795d90d02c75b256690a3ca07c97650
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-22 18:28:27 +0000

    Added server variable and main() call

--- src/testers/ServerTester.java
+++ src/testers/ServerTester.java
@@ -2,6 +2,8 @@ package testers;
 
 import org.junit.Test;
 
+import core.Server;
+
 public class ServerTester extends BasicTester {
 	
 	/**
@@ -9,6 +11,9 @@ public class ServerTester extends BasicTester {
 	 */
 	@Test
 	public void mainTester () {
+		Server server = new Server();
+		String[] args = new String[] {"4444"};
+		server.main(args);
 		test();
 	}
 }
