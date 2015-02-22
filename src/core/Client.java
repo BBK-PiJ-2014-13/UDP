02d9514ab0a187fe87736ef6d59eb05d741d3797
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-22 18:15:20 +0000

    Now sends or receives audio file

--- src/core/Client.java
+++ src/core/Client.java
@@ -2,7 +2,11 @@ package core;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -34,27 +38,32 @@ public class Client {
 			// Ask if its first to connect
 			outToServer.writeBytes("firstToConnectRequest");
 			firstToConnect = Boolean.parseBoolean(inFromServer.readLine());
-			
+
 			socket.close();
 
-			// TODO Open UDP connection to server
+			// Open UDP connection to server
 			DatagramSocket UDPsocket = new DatagramSocket();
 			InetAddress IPAddress = InetAddress.getByName("localhost");
 			byte[] sendData = new byte[1024];
 			byte[] receiveData = new byte[1024];
 			if (firstToConnect) {
-				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, portNumber);
+				DatagramPacket sendPacket = new DatagramPacket(sendData,
+						sendData.length, IPAddress, portNumber);
 				UDPsocket.send(sendPacket);
+				File audioFile = new File("Non, je ne regrette rien.mp3");
+				InputStream targetStream = new FileInputStream(audioFile);
+				targetStream.read(sendData);
+				targetStream.close();
 			} else {
-				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				DatagramPacket receivePacket = new DatagramPacket(receiveData,
+						receiveData.length);
 				UDPsocket.receive(receivePacket);
+				FileOutputStream fileOutputStream = new FileOutputStream(
+						"inputFile.mp3");
+				fileOutputStream.write(receivePacket.getData());
+				fileOutputStream.close();
 			}
 			UDPsocket.close();
-			
-
-			
-
-			// TODO Send or receive audio
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
