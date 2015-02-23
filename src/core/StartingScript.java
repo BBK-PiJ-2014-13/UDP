ded98ce2b8a06578af090689a03ba2a74081f6a9
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-23 16:30:20 +0000

    Created class StrtingScript

---
+++ src/core/StartingScript.java
@@ -0,0 +1,12 @@
+package core;
+
+/**
+ * @author Ilya Ivanov
+ *
+ *	Launches main methods of clients and server
+ */
+public class StartingScript {
+	public static void main(String[] args) {
+
+	}
+}
