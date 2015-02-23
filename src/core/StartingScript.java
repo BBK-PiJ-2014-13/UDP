854b47bbabae58fe19331dd8aeb8b71a1c6fc7a4
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-23 16:35:07 +0000

    Launched a server and a client

--- src/core/StartingScript.java
+++ src/core/StartingScript.java
@@ -7,6 +7,13 @@ package core;
  */
 public class StartingScript {
 	public static void main(String[] args) {
-
+		String portnumber = "1099";
+		String[] arguments = new String[] {portnumber};
+		
+		// Start a server
+		Server.main(arguments);
+		
+		// Start a client
+		Client.main(arguments);
 	}
 }
