5c4ac5fbd49d7eaa8e3a741c272eab6316dd9d29
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-26 16:18:56 +0000

    Removed unused while loop

--- src/core/Client.java
+++ src/core/Client.java
@@ -40,7 +40,7 @@ public class Client {
 			// Ask if its first to connect
 			outToServer.writeBytes("firstToConnectRequest" + "\n");
 			isFirstToConnect = Boolean.parseBoolean(inFromServer.readLine());
-			
+
 			// Send the name of file to Server
 			outToServer.writeBytes(fileName + "\n");
 
@@ -49,35 +49,32 @@ public class Client {
 			// UDP
 			DatagramSocket UDPsocket = new DatagramSocket();
 
-			while (true) {
-				InetAddress IPAddress = InetAddress.getByName("localhost");
-				byte[] sendData = new byte[1024];
-				byte[] receiveData = new byte[1024];
+			InetAddress IPAddress = InetAddress.getByName("localhost");
+			byte[] sendData = new byte[1024];
+			byte[] receiveData = new byte[1024];
 
-				// Send audio
-				if (isFirstToConnect) {
-					File audioFile = new File(fileName);
-					InputStream targetStream = new FileInputStream(audioFile);
-					while (targetStream.read(sendData) != -1) {
-						DatagramPacket sendPacket = new DatagramPacket(sendData,
-								sendData.length, IPAddress, portNumber);
-						UDPsocket.send(sendPacket);
-					}
-					targetStream.close();
+			// Send audio
+			if (isFirstToConnect) {
+				File audioFile = new File(fileName);
+				InputStream targetStream = new FileInputStream(audioFile);
+				while (targetStream.read(sendData) != -1) {
+					DatagramPacket sendPacket = new DatagramPacket(sendData,
+							sendData.length, IPAddress, portNumber);
+					UDPsocket.send(sendPacket);
 				}
+				targetStream.close();
+			}
 
-				// Receive audio
-				else {
-					// DatagramPacket receivePacket = new
-					// DatagramPacket(receiveData,
-					// receiveData.length);
-					// UDPsocket.receive(receivePacket);
-					// FileOutputStream fileOutputStream = new FileOutputStream(
-					// "inputFile.mp3");
-					// fileOutputStream.write(receivePacket.getData());
-					// fileOutputStream.close();
-				}
-				break;
+			// Receive audio
+			else {
+				// DatagramPacket receivePacket = new
+				// DatagramPacket(receiveData,
+				// receiveData.length);
+				// UDPsocket.receive(receivePacket);
+				// FileOutputStream fileOutputStream = new FileOutputStream(
+				// "inputFile.mp3");
+				// fileOutputStream.write(receivePacket.getData());
+				// fileOutputStream.close();
 			}
 			UDPsocket.close();
 		} catch (UnknownHostException e) {
