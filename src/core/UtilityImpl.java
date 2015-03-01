7f69dc6c86ff2be41eab91ad0e1c76eb3c3778e3
Ilya Ivanov <contact@ilyaivanov.org>
2015-03-01 01:24:03 +0000

    Added Socket to arguments

--- src/core/UtilityImpl.java
+++ src/core/UtilityImpl.java
@@ -1,9 +1,11 @@
 package core;
 
+import java.net.Socket;
+
 public class UtilityImpl implements Utility {
 
 	@Override
-	public String sendID() {
+	public String sendID(Socket socket) {
 		// TODO Auto-generated method stub
 		return null;
 	}
