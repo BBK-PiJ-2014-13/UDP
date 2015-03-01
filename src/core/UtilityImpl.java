09c346cfcadf0213e37114be5cb38b2d4f39d504
Ilya Ivanov <contact@ilyaivanov.org>
2015-03-01 01:28:27 +0000

    Added body to sendID()

--- src/core/UtilityImpl.java
+++ src/core/UtilityImpl.java
@@ -1,13 +1,30 @@
 package core;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class UtilityImpl implements Utility {
 
+	private BufferedReader inFromClient;
+	private DataOutputStream outToClient;
+
 	@Override
 	public String sendID(Socket socket) {
-		// TODO Auto-generated method stub
+		try {
+			inFromClient = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			outToClient = new DataOutputStream(socket.getOutputStream());
+			if (inFromClient.readLine().equals("IDrequest")) {
+				outToClient.writeBytes(Integer.toString(id) + "\n");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
-	
+
 }
